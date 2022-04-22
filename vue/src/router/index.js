bd14624f68c69951e791c1b3a2670b801843deab
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2022-04-22 08:03:17 +0100

    Redirect to Login page when not authorused0

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,13 +1,21 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Dashboard from '../views/Dashboard.vue';
+import store from "../store";
+import Surveys from '../views/Surveys.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
+import DefaultLayout from '../components/DefaultLayout.vue';
 
 const routes = [
     {
         path: '/',
-        name: 'Dashboard',
-        component: Dashboard
+        redirect: '/dashboard',
+        component: DefaultLayout,
+        meta: {requiresAuth: true},
+        children: [
+            {path: '/dashboard', name: 'Dashboard', component: Dashboard},
+            {path: '/surveys', name: 'Surveys', component: Surveys}
+        ]
     },
     {
         path: '/login',
@@ -24,6 +32,14 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if(to.meta.requiresAuth && !store.state.user.token){
+        next({name: 'Login'})
+    } else {
+        next();
+    }
 })
 
 export default router
