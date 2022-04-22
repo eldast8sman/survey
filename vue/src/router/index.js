094ed71514d85bbab2047b7ca244f895171dadc4
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2022-04-22 09:38:08 +0100

    Created AuthLayout Component and started modifying Registration and Login Pages

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,47 +1,56 @@
-import {createRouter, createWebHistory} from "vue-router";
-import Dashboard from '../views/Dashboard.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "../views/Dashboard.vue";
+import Surveys from "../views/Surveys.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
+import AuthLayout from "../components/AuthLayout.vue";
 import store from "../store";
-import Surveys from '../views/Surveys.vue';
-import Login from '../views/Login.vue';
-import Register from '../views/Register.vue';
-import DefaultLayout from '../components/DefaultLayout.vue';
 
 const routes = [
-    {
-        path: '/',
-        redirect: '/dashboard',
-        component: DefaultLayout,
-        meta: {requiresAuth: true},
-        children: [
-            {path: '/dashboard', name: 'Dashboard', component: Dashboard},
-            {path: '/surveys', name: 'Surveys', component: Surveys}
-        ]
-    },
-    {
-        path: '/login',
-        name: 'Login',
-        component: Login
-    },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
-    }
+  {
+    path: "/",
+    redirect: "/dashboard",
+    component: DefaultLayout,
+    meta: { requiresAuth: true },
+    children: [
+      { path: "/dashboard", name: "Dashboard", component: Dashboard },
+      { path: "/surveys", name: "Surveys", component: Surveys }
+    ],
+  },
+  {
+    path: "/auth",
+    redirect: "/login",
+    name: "Auth",
+    component: AuthLayout,
+    children: [
+      {
+        path: "/login",
+        name: "Login",
+        component: Login,
+      },
+      {
+        path: "/register",
+        name: "Register",
+        component: Register,
+      },
+    ],
+  }
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
 router.beforeEach((to, from, next) => {
-    if(to.meta.requiresAuth && !store.state.user.token){
-        next({name: 'Login'});
-    } else if(store.state.user.token && (to.name === "Login" || to.name === "Register")){
-        next({name: "Dashboard"});
-    } else {
-        next();
-    }
-})
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({ name: "Login" });
+  } else if (store.state.user.token && (to.name === "Login" || to.name === "Register")) {
+    next({ name: "Dashboard" });
+  } else {
+    next();
+  }
+});
 
-export default router
+export default router;
