a9e37881817f6e1760c95575431c6f9d23d4b212
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2022-04-23 05:25:39 +0100

    Worked on Navigation

--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
     <header class="bg-white shadow">
       <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold text-gray-900">Dashboard</h1>
@@ -13,6 +14,7 @@
         <!-- /End replace -->
       </div>
     </main>
+  </div>
 </template>
 
 <script>
