7dd13cb49d02ad3cb70aef3eed4a267975861a00
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2022-04-30 05:14:48 +0100

    Login SessionSotirage sorted

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -30,7 +30,7 @@
           <div>
             <label for="password_confirmation" class="sr-only">Password Confirmation</label>
             <input id="password_confirmation" name="password_confirmation" type="password" autocomplete="current-password" required="" v-model="user.password_confirmation"
-            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
+            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Confirm Password" />
           </div>
         </div>
 
