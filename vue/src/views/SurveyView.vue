3c9a3d8670fdf0649369f4aec32105e77a72243d
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2022-05-08 03:26:30 +0100

    Question Part of SurveyView was worked on

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -7,9 +7,6 @@
                 </h1>
             </div>
         </template>
-        <pre>
-            {{model}}
-        </pre>
         <form @submit.prevent="saveSurvey">
             <div class="shadow sm:rounded-md sm:overflow-hidden">
                   
@@ -193,6 +190,57 @@
                             </div>
                         </div>
                         <!--/ Status -->
+                  </div>
+                  <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+                      <h3 class="text-2l font-semibold flex items-center justify-between">
+                        Questions
+
+                        <!-- Add New Question -->
+                        <button
+                            type="button"
+                            @click="addQuestion()"
+                            class="
+                                flex
+                                items-center
+                                text-sm
+                                py-1
+                                px-4
+                                rounded-sm
+                                text-white
+                                bg-gray-600
+                                hover:bg-gray-700
+                            "
+                        >
+                            <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                class="h-4 w-4 -mt-1 inline-block" 
+                                fill="none" 
+                                viewBox="0 0 24 24" 
+                                stroke="currentColor" 
+                            >
+                                <path 
+                                stroke-linecap="round" 
+                                stroke-linejoin="round" 
+                                stroke-width="2"
+                                d="M12 4v16m8-8H4" 
+                                />
+                            </svg>
+                            Add Question
+                        </button>
+                        <!--/ Add New Question -->
+                      </h3>
+                      <div v-if="!model.questions.length" class="text-center text-gray-600">
+                          You don't have any questions created
+                      </div>
+                      <div v-for="(question, index) in model.questions" :key="question.id">
+                          <QuestionEditor
+                            :question="question"
+                            :index="index"
+                            @change="questionChange"
+                            @addQuestion="addQuestion"
+                            @deleteQuestion="deleteQuestion"
+                          />
+                      </div>
                   </div>
                   <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
                       <button
